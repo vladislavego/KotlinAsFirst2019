1389c5e3b40b8388c59388015529926e7d401350
vladislavego <ego.v.a@ya.ru>
2019-09-21 16:50:03 +0300

    решена задача 2(lengthInMeters)

--- src/lesson1/task1/Simple.kt
+++ src/lesson1/task1/Simple.kt
@@ -71,7 +71,11 @@ fun seconds(hours: Int, minutes: Int, seconds: Int): Int {
  * Определить длину того же отрезка в метрах (в данном случае 18.98).
  * 1 сажень = 3 аршина = 48 вершков, 1 вершок = 4.445 см.
  */
-fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int): Double = TODO()
+fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int): Double {
+    val inVershoks = sagenes * 48 + arshins * 48 / 3 + vershoks
+    val inMeters = inVershoks * 4.445 / 100
+    return inMeters
+}
 
 /**
  * Тривиальная
