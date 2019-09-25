733e00c875d02291a2c45b6e34dfbdc512d078c7
vladislavego <ego.v.a@ya.ru>
2019-09-25 17:47:57 +0300

    lesson2.task2.brickPasses solve change

--- src/lesson2/task2/Logical.kt
+++ src/lesson2/task2/Logical.kt
@@ -66,4 +66,5 @@ fun circleInside(
  * Вернуть true, если кирпич пройдёт
  */
 fun brickPasses(a: Int, b: Int, c: Int, r: Int, s: Int): Boolean = (a * b < r * s || a * c < r * s ||
-        b * c < r * s || (a == r || b == r || c == r && a == s || b == s || c == s))
+        b * c < r * s || a == r && b == s || a == s && b == r || b == r && c == s || b == s && c == r ||
+        a == r && c == s || a == s && c == r)
