b4821ba7965c2c48234a4e4886d72ddecef766ce
vladislavego <ego.v.a@ya.ru>
2019-09-28 15:32:27 +0300

    lesson3.task1.digitNumber решение

--- src/lesson3/task1/Loop.kt
+++ src/lesson3/task1/Loop.kt
@@ -67,7 +67,15 @@ fun digitCountInNumber(n: Int, m: Int): Int =
  *
  * Использовать операции со строками в этой задаче запрещается.
  */
-fun digitNumber(n: Int): Int = TODO()
+fun digitNumber(n: Int): Int {
+    var count = 0
+    var number = n
+    do {
+        number /= 10
+        count += 1
+    } while (number != 0)
+    return count
+}
 
 /**
  * Простая
