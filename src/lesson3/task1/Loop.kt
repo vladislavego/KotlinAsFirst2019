505daeab623b20b702b7b361f3d4d9a4ed8642ec
vladislavego <ego.v.a@ya.ru>
2019-09-28 23:47:22 +0300

    lesson3.task1.collatzSteps решение

--- src/lesson3/task1/Loop.kt
+++ src/lesson3/task1/Loop.kt
@@ -196,7 +196,16 @@ fun squareBetweenExists(m: Int, n: Int): Boolean {
  * Написать функцию, которая находит, сколько шагов требуется для
  * этого для какого-либо начального X > 0.
  */
-fun collatzSteps(x: Int): Int = TODO()
+fun collatzSteps(x: Int): Int {
+    var number = x
+    var steps = 0
+    while (number != 1) {
+        if (number % 2 == 0) number /= 2
+        else number = number * 3 + 1
+        steps += 1
+    }
+    return steps
+}
 
 /**
  * Средняя
