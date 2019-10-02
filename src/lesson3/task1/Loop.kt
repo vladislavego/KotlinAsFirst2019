a98024684b7df98c677f1a4413b0042baa63429c
vladislavego <ego.v.a@ya.ru>
2019-10-02 19:22:08 +0300

    lesson3.task1.isPalindrome

--- src/lesson3/task1/Loop.kt
+++ src/lesson3/task1/Loop.kt
@@ -251,7 +251,7 @@ fun revert(n: Int): Int {
  *
  * Использовать операции со строками в этой задаче запрещается.
  */
-fun isPalindrome(n: Int): Boolean = TODO()
+fun isPalindrome(n: Int): Boolean = n == revert(n)
 
 /**
  * Средняя
