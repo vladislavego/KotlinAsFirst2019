aaa24920ab0bef6c966739997bfeadfb033447aa
vladislavego <ego.v.a@ya.ru>
2019-10-30 16:26:50 +0300

    lesson6. решена задача flattenPhoneNumber

--- src/lesson6/task1/Parse.kt
+++ src/lesson6/task1/Parse.kt
@@ -158,7 +158,18 @@ fun dateDigitToStr(digital: String): String {
  * PS: Дополнительные примеры работы функции можно посмотреть в соответствующих тестах.
  */
 
-fun flattenPhoneNumber(phone: String): String = TODO()
+fun flattenPhoneNumber(phone: String): String =
+    try {
+        if (phone[0] == '+') {
+            "+" + phone.filter { it != ' ' && it != '-' && it != '+' }.split("(", ")")
+                .map { it.toInt() }.joinToString(separator = "")
+        } else {
+            phone.filter { it != ' ' && it != '-' && it != '+' }.split("(", ")")
+                .map { it.toInt() }.joinToString(separator = "")
+        }
+    } catch (e: NumberFormatException) {
+        ""
+    }
 
 /**
  * Средняя
